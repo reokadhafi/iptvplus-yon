2e6c320475ab1055b165886e5391af221f5bd7b8
reokadhafi <106902195+reokadhafi@users.noreply.github.com>
2025-04-08 15:02:33 +0900

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 import re
 
+
 options = Options()
 options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
